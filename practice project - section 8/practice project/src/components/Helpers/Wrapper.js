bc3b639369a2f968fab51c262428ec15036ca784
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-05 10:42:17 +0200

    portals

---
+++ practice project - section 8/practice project/src/components/Helpers/Wrapper.js
@@ -0,0 +1,5 @@
+const Wrapper = props => {
+    return props.children;      // children sadrzi sav sadrzaj koji se nalazi izmedju tagova trenutne komponente
+};
+
+export default Wrapper;
--- practice project - section 8/practice project/src/components/Users/AddUser.js
+++ practice project - section 8/practice project/src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import React, { useState } from "react";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [eneteredUsername, setEnteredUsername] = useState("");
@@ -46,7 +47,7 @@ const AddUser = (props) => {
   }
 
   return (
-    <div>
+    <Wrapper>
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
@@ -67,7 +68,7 @@ const AddUser = (props) => {
           <Button type="submit">Add user</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
