b0a2186b9dbe9b8adb57fa0fcb385f5e932ff7a8
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-08 09:43:00 +0200

    useReducer & useEffect
    cilj je pokrenuti useEffect samo ako se validacija forme promeni, a ne prilikom svake promene input polja -> to se radi izdvajanjem property-a

--- side efect - section 10/src/components/Login/Login.js
+++ side efect - section 10/src/components/Login/Login.js
@@ -6,26 +6,42 @@ import Button from '../UI/Button/Button';
 
 // moze da se pise izvan login komponente jer ne interaguje ni sa cim unutar login komponente
 const emailReducer = (state, action) => {       // drugi arg je f-ja koja ce pozvati akciju koja ce biti consumed by prvi arg
-  if(action.type === 'USER_INPUT') {
+  if(action.type === 'USER_INPUT') {      // ono sto saljemo kao action ce biti objekat jer smo tako postavili u 54. liniji, a taj objekat ima type polje
     return { value: action.val, isValid: action.val.includes('@') };
   }  
   if(action.type === 'INPUT_BLUR') {
-    return { value: state.value, isValid: state.value.includes('@') };
+    return { value: state.value, isValid: state.value.includes('@') };    // za INPUT_BLUR ne menjamo value, pa ovde preuzimamo najnoviju vrednost pomocu state.value 
   }
   return { value: '', isValid: false };
 };
 
+const passwordReducer = (state, action) => {     
+  if(action.type === 'USER_INPUT') {     
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }  
+  if(action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: state.value.trim().length > 6 };   
+  }
+  return { value: '', isValid: false };
+};
+
+
 const Login = (props) => {
  // const [enteredEmail, setEnteredEmail] = useState('');
  // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+//  const [enteredPassword, setEnteredPassword] = useState('');
+ // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
      value: '',                // initial state for emailState
      isValid: null,
   });
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: '',
+    isValid: null,
+  })
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -35,35 +51,39 @@ const Login = (props) => {
     };
   }, []);
 
-/*
+   // prednost izdvajanja isValid-a je ta sto se useEffect ne pokrece ako se samo vrednost input polja promeni, a validacija ostane ista (true)
+   const { isValid: emailIsValid } = emailState;         // object destructuring - koristi se da bi se izvukao property iz objekta
+   const { isValid: passwordIsValid } = passwordState;  // isValid property smestamo u alijas pod proizvoljnim nazivom, sto je ustvari konstanta
+
+
   useEffect(() => {                              // pomocu ove f-je se dugme za login disable-uje i enabluje direktno prilikom menjanja pass ili username-a
     const identifier = setTimeout(() => {
       console.log('Checking form validity!');
-    setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+    setFormIsValid(emailIsValid && passwordIsValid);
   }, 500);                                     // ceka se da korisnik prestane sa kucanjem najmanje 500ms kako bi tek onda proverio da li uneti username postoji,
-                                              // a to se radi zbog optimizacije kako ne bi vrsio proveru posle svakog unetog karaktera 
+                                           //   a to se radi zbog optimizacije kako ne bi vrsio proveru posle svakog unetog karaktera 
     return () => {                            // cleanup f-ja koja se pokrece svaki put pre izvrsenja ove gore f-je
       console.log('CLEANUP');
       clearTimeout(identifier);              // f-ja koja je ugradjena u browser
     };    
   },
-  [setFormIsValid, enteredEmail, enteredPassword]);  // u dependecies se dodaje ono sto ide u sideEffect f-ji
-                                                    // pokrenuce sideEffect f-ju ako je setFormIsValid true ili enteredEmail ili enteredPass promenjeni
-*/
+  [emailIsValid, passwordIsValid]);  // u dependecies se dodaje ono sto ide u sideEffect f-ji
+                                 //  pokrenuce sideEffect f-ju ako su emailIsValid ili passwordIsValid promenjeni
+
   const emailChangeHandler = (event) => {
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    )
+    // setFormIsValid(
+    //   event.target.value.includes('@') && passwordState.isValid
+    // )
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({type: 'USER_INPUT', val: event.target.value});
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && emailState.isValid
-    );
+    // setFormIsValid(
+    //   event.target.value.trim().length > 6 && emailState.isValid
+    // );
   };
 
   const validateEmailHandler = () => {
@@ -71,12 +91,12 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchEmail({type: 'INPUT_BLUR'});
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -98,14 +118,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
