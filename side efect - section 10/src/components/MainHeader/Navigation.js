1a1e9a865f54349e8d94e2c702f06269f80455a8
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-08 12:02:42 +0200

    useContext()

--- side efect - section 10/src/components/MainHeader/Navigation.js
+++ side efect - section 10/src/components/MainHeader/Navigation.js
@@ -1,37 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
-    // to consume (or use) what was provided on our context, we use the consumer component
-    // ne stavljati space posle 10. linije jer se parsira kao child ka Consumer-u 
-    <AuthContext.Consumer>
-      {(ctx) => {             // ctx je context data koji je ustvari objekat iz auth-context
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
