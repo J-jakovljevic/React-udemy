81f76af59360ff5e03fc91e59f8a1cb415704740
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-10 13:33:27 +0200

    added class-based component
    class-based component is an old way of creating components which are using render function, but today we're using functional components where we're using just return statement

--- class-based components - section 13/src/components/User.js
+++ class-based components - section 13/src/components/User.js
@@ -1,7 +1,14 @@
 import classes from './User.module.css';
+import { Component } from 'react';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  render() {                                // ekvivalentno sa return statement u functional components
+    return <li className={classes.user}>{this.props.name}</li>;     // this moze samo zbog extents Component
+  };
+}
+
+/*const User = (props) => {               -->   functional component
+    return <li className={classes.user}>{props.name}</li>;
+};*/
 
 export default User;
