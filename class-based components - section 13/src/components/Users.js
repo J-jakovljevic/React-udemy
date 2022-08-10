7009ca0a4c3de6220f10f8b6a713a9828c6e53f4
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-10 13:59:05 +0200

    working with state
    with class-based components, state is always an object
    with functional components, state can be anything

--- class-based components - section 13/src/components/Users.js
+++ class-based components - section 13/src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,6 +9,45 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
+class Users extends Component {
+  constructor() {           // here we do initialization
+    super();
+    this.state = {          // state is always an object!!
+      showUsers: true
+    };        
+  }
+
+  toggleUsersHandler() {
+    // this.state.showUsers = false;    // NOT!
+    this.setState((curState) => {         // react merge this update with existing state (in constructor)
+                                         // (it doesn't override the state like in useState)
+        return { showUsers: !curState.showUsers };
+    });     
+                                          
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>   {/* we're adding bind() bcs .this must refers to the surrounding class
+                                                                and that is not done by default when method is called upon event like click*/}
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+/*
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
@@ -33,5 +72,6 @@ const Users = () => {
     </div>
   );
 };
+*/
 
 export default Users;
