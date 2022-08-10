9f4c09cf2c3102ade90debeac64f2dc1af29c758
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-10 09:28:24 +0200

    removing items from cart

--- food order app - section 11/src/store/CartProvider.js
+++ food order app - section 11/src/store/CartProvider.js
@@ -1,6 +1,7 @@
 // cilj ove komponente je da upravlja contexom i da ga obezbedi svim komponentama koje zele pristup contextu
-import CartContext from "./cart-context";
 import { useReducer } from "react";
+import CartContext from "./cart-context";
+
 
 const defaultCartState = {
   items: [],
@@ -9,8 +10,7 @@ const defaultCartState = {
 
 // moze van jer nece koristiti nista sto je unutar f-je ove komponente
 const cartReducer = (state, action) => {
-  if (action.type === "ADD") {
-    // grupisemo iteme za isto meso zajedno    
+  if (action.type === "ADD") { 
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
@@ -37,8 +37,31 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+
   return defaultCartState;
-};
+}
 
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
