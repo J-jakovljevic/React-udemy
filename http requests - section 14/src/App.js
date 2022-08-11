f3cf721c20f70b44d319067fcbd86963cad8402b
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-11 11:35:56 +0200

    GET request

--- http requests - section 14/src/App.js
+++ http requests - section 14/src/App.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
 
+  function fetchMoviesHandler() {
+    fetch('https://swapi.dev/api/films').then((response) => {   // method for fetch api (default method is get) which returns
+                                                              // promise bcs sending htpp req.is asynchronous which means 
+                                                              // it doesn't finish immediately -> response will be there
+                                                              // in the future -> we need promise
+                                                              // .then will be called whenever we got response
+      return response.json();      // transforms json response body to js object and returns a promise
+    }).then((data) => {             // here we got our transformed data 
+      const transformedMovies = data.results.map(movieData => {  // .map() - converting every object in the results array into a new object
+        return {
+          id: movieData.episode_id,               // data we're working with in front : data we get back from json
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMovies(transformedMovies);
+    });     
+  }  
+  
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
