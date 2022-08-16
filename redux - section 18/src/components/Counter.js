35dee3d050abdd7b5ed5f4592ea17919787cb033
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-16 12:52:12 +0200

    refactoring code using class-based components
    useDispatch and useSelector are hooks and hooks are not allowed in class-based components, so we use { connect }, which allows class-based component to connect to redux. Connect also set up subscription automatically.

--- redux - section 18/src/components/Counter.js
+++ redux - section 18/src/components/Counter.js
@@ -1,6 +1,9 @@
+import { Component } from 'react';
 import classes from './Counter.module.css';
-import { useSelector, useDispatch } from 'react-redux/es/exports';   // useStore gives access to the store, but useSelector allows us to 
-                                                                    // automatically select a part of our state managed by the store 
+import { useSelector, useDispatch, connect } from 'react-redux/es/exports';   // useStore gives access to the store, but useSelector allows us to 
+                                                                             // automatically select a part of our state managed by the store 
+                                                                            // { connect } allows class-based component to connect to redux
+ /*                                                                     
 const Counter = () => {
   const dispatch = useDispatch();   // returns a function which will dispatch an action against redux store
   
@@ -32,5 +35,50 @@ const Counter = () => {
     </main>
   );
 };
+*/
+class Counter extends Component {
+  incrementHandler() {
+    // in counter comp. in his props we have prop named increment (in mapDispatchToProps)
+    this.props.increment();
+  }
 
-export default Counter;
+  decrementHandler() {
+    this.props.decrement();
+  }
+
+  toggleCounterHandler() {
+
+  }
+
+  render() {
+    return (
+      <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{this.props.counter}</div>   {/* this.props bcs of mapStateToProps */}
+      <div>
+        <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+        <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+      </div>
+      <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+    </main>
+    )
+  }
+}
+
+const mapStateToProps = state => {     // function that maps redux state to props
+  return {                            // returns object where keys'll be available as props in the receiving component (counter comp.)
+    counter: state.counter           // counter is name up to u
+  }
+}
+
+// function which is used to store dispatch functions in props (in Counter comp. we have certain props which we can execute as a function
+// which will then - when executed - dispatch an action to redux store)
+const mapDispatchToProps = dispatch => {
+  return {
+      increment: () => dispatch({ type: 'increment' }),  // keys are props names which we can use in counter comp.
+      decrement: () => dispatch({ type: 'decrement' }),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);    // we execute the connect function, then it returns a new function which 
+                                                                        //we execute -> to that returned function we pass counter
