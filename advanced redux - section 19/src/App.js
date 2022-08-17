7fb9fd3756bd9a4fdb14a8b67f623909f878d3a0
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-17 13:36:43 +0200

    handling with response and with errors using action creator

--- advanced redux - section 19/src/App.js
+++ advanced redux - section 19/src/App.js
@@ -4,8 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -18,48 +18,11 @@ function App() {
   // it's not allowed useEffect(aync() => {},[]) because when you mark a function with async it will implicitly return a promise.
   // So when it comes to running the clean up function returned from useEffect you'll also be implicitly returning a promise, which isn't allowed as this can cause race conditions.
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://react-http-17999-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT", // put method will override the existing data, instead of post method which would add new data to a list of data
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
-    if (isInitial) {      // this blocks sending cart data (function below) when the app starts
+    if (isInitial) {
       isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]); // method will reexecutes whenever cart changes
 
   return (
