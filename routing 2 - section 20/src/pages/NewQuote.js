070dfa280a2b49017a01d1462735ff77c785487d
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-18 15:42:15 +0200

    redirecting to new page on button click with useHistory()
    useHistory hook allows us to change the browser history (like navigating us to new page); it gives us history object on which could be used push and replace methods. With push we can go back to previous page (with the back button) and with replace we can't.

--- routing 2 - section 20/src/pages/NewQuote.js
+++ routing 2 - section 20/src/pages/NewQuote.js
@@ -1,8 +1,14 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+    const history = useHistory();  // useHistory allows us to change the browser history (like navigating us to new page)
+    
     const addQuoteHandler = quoteData => {
         console.log(quoteData);
+
+        history.push('/quotes');
     }
 
     return <QuoteForm onAddQuote={addQuoteHandler}/>
