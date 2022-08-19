217b2a4b00de408f2a097093ea54f58836ea2ce0
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-19 11:27:10 +0200

    using nested routes for comments section

--- routing 2 - section 20/src/pages/QuoteDetail.js
+++ routing 2 - section 20/src/pages/QuoteDetail.js
@@ -1,26 +1,33 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Joksi', text: 'Some text' },
-    { id: 'q2', author: 'Joksi 2', text: 'Some text2' },
+  { id: "q1", author: "Joksi", text: "Some text" },
+  { id: "q2", author: "Joksi 2", text: "Some text2" },
 ];
 
 const QuoteDetail = () => {
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);    // is id quote from array = id quote from url ("quoteId" is set to route in app.js)
-  
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId); // is id quote from array = id quote from url ("quoteId" is set to route in app.js)
+
   if (!quote) {
-    return <p>No quote found!</p>
+    return <p>No quote found!</p>;
   }
-  
+
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
-      <Route path={`/quotes/${params.quoteId}/comments`}>    {/* same as path={'/quotes/${params.quoteId}/comments} */}
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>   
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
     </Fragment>
