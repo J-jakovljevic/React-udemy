bb3207dc40c60f1dc4c7bc7643b93de34958e6c8
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-22 15:53:41 +0200

    changing password

--- authentication - section 22/src/components/Profile/ProfileForm.js
+++ authentication - section 22/src/components/Profile/ProfileForm.js
@@ -1,11 +1,41 @@
+import { useContext, useRef } from 'react';
+
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const newPassInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPass = newPassInputRef.current.value;
+
+    // optional: add validation
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyALo34yPd4UvL70JncBzikj5cs12dgFSDc', 
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPass,
+        returnSecureToken: false // required on firebase auth rest api website for changing pass
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      // pretpostavka da je uvek succeed kako bismo ustedeli vreme (nema error hendlinga)
+      
+    })
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength="7" ref={newPassInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
