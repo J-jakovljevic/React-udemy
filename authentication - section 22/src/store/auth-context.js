a1f639cd56cae34576258107b7c272c4618e01be
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-22 15:03:23 +0200

    changing navigation bar using context

---
+++ authentication - section 22/src/store/auth-context.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+    token: '',
+    isLoggedIn: false,
+    login: (token) => {},
+    logout: () => {}
+});
+
+export const AuthContextProvider = (props) => {
+    const [token, setToken] = useState(null);
+
+    const userIsLoggedIn = !!token;     // if token is a string that's not empty -> this'll return true, otherwise is false
+
+    const loginHandler = (token) => {
+        setToken(token);
+    };
+
+    const logoutHandler = () => {
+        setToken(null);
+    };
+
+    const contexValue = {
+        token: token,
+        isLoggedIn: userIsLoggedIn,
+        login: loginHandler,
+        logout: logoutHandler
+    }
+
+    return <AuthContext.Provider value={contexValue}>{props.children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
