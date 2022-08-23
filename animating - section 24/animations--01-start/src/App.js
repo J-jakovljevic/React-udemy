0091ca51bd6e33eb36c1d57b9a5d2faa1a732fff
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-23 15:26:18 +0200

    react_transition_group package - part 1
    allows us to animate elements when they are added and removed to and from the dom

--- animating - section 24/animations--01-start/src/App.js
+++ animating - section 24/animations--01-start/src/App.js
@@ -7,24 +7,38 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
-  }
+    modalIsOpen: false,
+    showBlock: false,
+  };
 
   showModal = () => {
-    this.setState({modalIsOpen: true});
-  }
+    this.setState({ modalIsOpen: true });
+  };
 
   closeModal = () => {
-    this.setState({modalIsOpen: false});
-  }
+    this.setState({ modalIsOpen: false });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />   {/* this is bad for performance */}
-        <Backdrop show={this.state.modalIsOpen}/>
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        <button className="Button" onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
+        <br />
+        {this.state.showBlock ? (
+          <div 
+          style={{ 
+            backgroundColor: "red", 
+            width: 100, 
+            height: 100,
+            margin: 'auto'
+          }} />
+        ) : null}
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> {/* this is bad for performance */}
+        <Backdrop show={this.state.modalIsOpen} />
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
