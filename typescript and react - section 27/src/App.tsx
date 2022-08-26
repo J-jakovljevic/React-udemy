2ee6188166c6e7b32610636d93672c680493f7fe
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-26 10:15:08 +0200

    refactoring by adding a data model

--- typescript and react - section 27/src/App.tsx
+++ typescript and react - section 27/src/App.tsx
@@ -1,9 +1,15 @@
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [         // isn't anymore array of strings, it's array of todo objects
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript')
+  ];
+
   return (
     <div>
-      <Todos items={['Lear React', 'Learn TypeScript']}/>
+      <Todos items={todos}/>
     </div>
   );
 }
--- typescript and react - section 27/src/components/Todos.tsx
+++ typescript and react - section 27/src/components/Todos.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Todo from "../models/todo";
 
-const Todos: React.FC<{items: string[]}>= (props) => {     // fc makes it clear that this here is a function that asks as a functional component
+const Todos: React.FC<{items: Todo[]}>= (props) => {     // fc makes it clear that this here is a function that asks as a functional component
                                                          //fc type is generic and the value we're plugging in is our own props object where we 
                                                         // describe our own props for this specific functional component; it's generic bcs
                                                         // different functional components have different props definitions
@@ -8,7 +9,7 @@ const Todos: React.FC<{items: string[]}>= (props) => {     // fc makes it clear 
     return (
         <ul>
             {props.items.map((item) => (
-                <li key={item}>{item}</li>
+                <li key={item.id}>{item.text}</li>
             ))}
         </ul>
     )
