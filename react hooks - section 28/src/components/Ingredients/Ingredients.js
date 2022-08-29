c4ba6f954730125fbcba849de2800bd6a7c8ff28
Jovana Jakovljević <Jovanajako@hotmail.com>
2022-08-29 10:45:50 +0200

    search update
    infinite loop was caused by onLoadIngredients dependency in useEffect in Search.js
    
    we don't want to send request on every keystroke bcs of performance -> we solved this with setTimeout, but that also has a problem bcs we're sending a lot of timers -> we solve this with cleanUp function: we clean up the old effect, run a new one on the second keystroke, we clean up a previous effect and run a new on

--- react hooks - section 28/src/components/Ingredients/Ingredients.js
+++ react hooks - section 28/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,25 +7,11 @@ import Search from './Search';
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  useEffect(() => {
-    fetch('https://react-hooks-update-3b031-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json')
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients = [];
-      for(const key in responseData) {
-        loadedIngredients.push({
-          id: key,  // random string we have in firebase
-          title: responseData[key].title, // responseData is nested for every key
-          amount: responseData[key].amount
-        })
-      }
-      setUserIngredients(loadedIngredients);
-    })
-  }, []);
-
-  const filteredIngredientsHandler = filteredIngredients => {
+  // we don't need useEffect bcs we already fetching ingredients in search component
+  
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
     setUserIngredients(filteredIngredients);
-  }
+  }, []);   // we have no dependency bcs setUserIngredients is special dependency bcs of useState
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-3b031-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json',
--- react hooks - section 28/src/components/Ingredients/Search.js
+++ react hooks - section 28/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -7,30 +7,39 @@ const Search = React.memo(props => {
   const { onLoadIngredients } = props;   // object destructuring -> {name of the keys in that object which we want to extract, in which we want to store and separate variables } = object we want to destructor or from which we want to extract
                                       // here, we know that onLoadIngredients exists as a key in props (bcs of line 24)
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;   // title is the filed from which we want to filter 
-    fetch('https://react-hooks-update-3b031-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + query)
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients = [];
-      for(const key in responseData) {
-        loadedIngredients.push({
-          id: key,  // random string we have in firebase
-          title: responseData[key].title, // responseData is nested for every key
-          amount: responseData[key].amount
-        })
-      }
-       onLoadIngredients(loadedIngredients);   // we expect onLoadIngredients() in Ingredients.js
-    })
-  }, [enteredFilter, onLoadIngredients]);  // useEffect runs everytime this fields change
+  const timer = setTimeout(() => {
+    if(enteredFilter === inputRef.current.value) {
+      const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;   // title is the filed from which we want to filter 
+      fetch('https://react-hooks-update-3b031-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' + query)
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for(const key in responseData) {
+          loadedIngredients.push({
+            id: key,  // random string we have in firebase
+            title: responseData[key].title, // responseData is nested for every key
+            amount: responseData[key].amount
+          })
+        }
+         onLoadIngredients(loadedIngredients);   // we expect onLoadIngredients() in Ingredients.js
+      })
+    }
+  }, 500);
+  return () => {    // if we want useEffect to have a return value, it's always a function
+      // it's a clean up function: we clean up the old effect, run a new one on the second keystroke, we clean up a previous effect and run a new on
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadIngredients, inputRef]);  // useEffect runs everytime this fields change
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)} />
+          <input ref={inputRef} type="text" value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)} />
         </div>
       </Card>
     </section>
